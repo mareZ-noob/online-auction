90d60ce029ffa6acb610fcdd773913ffa3a4f33c
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2025-11-22 16:19:44 +0700

    feat (frontend): add signin and signup UI

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,9 +1,33 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
+import ErrorPage from "@/layouts/errorPage.tsx";
+import {lazy, Suspense} from "react";
+import LoadingPage from "@/layouts/loadingPage.tsx";
+
+const SignInPage = lazy(() => import('@/components/auth-page/SignInPage'))
+const SignUpPage = lazy(() => import('@/components/auth-page/SignUpPage.tsx'))
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: (<h1>Welcome</h1>)
+        path: '/auth',
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Navigate to="sign-in" replace />,
+            },
+            {
+                path: 'sign-in',
+                element: (<Suspense fallback={<LoadingPage />}>
+                    <SignInPage />
+                </Suspense>),
+            },
+            {
+                path: 'sign-up',
+                element: (<Suspense fallback={<LoadingPage />}>
+                    <SignUpPage />
+                </Suspense>),
+            }
+        ]
     }
 ]);
 
