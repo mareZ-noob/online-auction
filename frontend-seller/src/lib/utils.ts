e3d237e7e94216ca89d7f8cdb7ab7084d7507866
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2025-12-09 14:11:11 +0000

    chore: remove redundant folder project for selller

--- frontend-seller/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClient } from "@tanstack/react-query";
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export const queryClient = new QueryClient();
