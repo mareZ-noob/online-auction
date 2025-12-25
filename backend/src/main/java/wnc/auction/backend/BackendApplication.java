07c8e37eb0d1203f2c093827a38d09ddf9134582
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2025-12-25 21:36:19 +0700

    feat: add heartbeat for sse

--- backend/src/main/java/wnc/auction/backend/BackendApplication.java
+++ backend/src/main/java/wnc/auction/backend/BackendApplication.java
@@ -1,11 +1,10 @@
 package wnc.auction.backend;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
